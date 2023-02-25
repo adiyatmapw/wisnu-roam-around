faa8c4eaa303c42c29042970c8a960e8a18ec476
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-02-24 16:13:11 -0800

    remove gtm

--- app/head.tsx
+++ app/head.tsx
@@ -7,13 +7,6 @@ export default function Head() {
       <link rel="icon" href="/favicon.ico" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7265051125086713"
      crossOrigin="anonymous"></script>
-     <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NP7DL37');</script>
-      <!-- End Google Tag Manager -->
     </>
   )
 }
