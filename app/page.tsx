c6b0aced439756c66e0d5e36761ee51a18012423
CXoXo-io <126208733+CXoXo-io@users.noreply.github.com>
2023-02-25 01:46:44 -0500

    vercel audiences5 removed function

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,6 @@ import type { AppProps } from 'next/app';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
-};
-
-export default MyApp;
-
-
 export default function Home() {
   const [request, setRequest] = useState<{days?: string, city?: string}>({})
   let [itinerary, setItinerary] = useState<string>('')
